74770dfa99894a819617310a646a67039bfc374e
Patrik Csak <p@trikcsak.com>
2022-12-06 12:20:37 -0800

    add day 3 part 2

---
+++ 3-rucksack-reorganization/solve-part-2.js
@@ -0,0 +1,38 @@
+import {readFile} from 'node:fs/promises';
+
+function getPriority(item) {
+	if (/[a-z]/.test(item)) return item.codePointAt(0) - 'a'.codePointAt(0) + 1;
+	if (/[A-Z]/.test(item)) return item.codePointAt(0) - 'A'.codePointAt(0) + 27;
+}
+
+async function solve() {
+	const input = await readFile(new URL('input.txt', import.meta.url), {
+		encoding: 'utf8',
+	});
+
+	const rucksacks = input.split('\n');
+
+	const groups = [];
+
+	for (let i = 0; i < rucksacks.length; i++) {
+		if ((i + 1) % 3 === 0) {
+			groups.push([rucksacks[i - 2], rucksacks[i - 1], rucksacks[i]]);
+		}
+	}
+
+	const badges = [];
+
+	for (const group of groups) {
+		badges.push(
+			[...group[0]].find(
+				(item) => group[1].includes(item) && group[2].includes(item),
+			),
+		);
+	}
+
+	return badges
+		.map((badge) => getPriority(badge))
+		.reduce((sum, priority) => sum + priority);
+}
+
+console.log(await solve());
