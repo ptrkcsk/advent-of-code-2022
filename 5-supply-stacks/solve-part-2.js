292dc5fb1597b1fcb9dcc54f71ac431ecafbd3b4
Patrik Csak <p@trikcsak.com>
2022-12-06 13:33:05 -0800

    add day 5 part 2

---
+++ 5-supply-stacks/solve-part-2.js
@@ -0,0 +1,36 @@
+import {readFile} from 'node:fs/promises';
+
+const input = await readFile(new URL('input.txt', import.meta.url), {
+	encoding: 'utf8',
+});
+
+const stacks = [
+	['Q', 'S', 'W', 'C', 'Z', 'V', 'F', 'T'],
+	['Q', 'R', 'B'],
+	['B', 'Z', 'T', 'Q', 'P', 'M', 'S'],
+	['D', 'V', 'F', 'R', 'Q', 'H'],
+	['J', 'G', 'L', 'D', 'B', 'S', 'T', 'P'],
+	['W', 'R', 'T', 'Z'],
+	['H', 'Q', 'M', 'N', 'S', 'F', 'R', 'J'],
+	['R', 'N', 'F', 'H', 'W'],
+	['J', 'Z', 'T', 'Q', 'P', 'R', 'B'],
+];
+
+const instructions = input
+	.split('\n')
+	.slice(10)
+	.map((instruction) => {
+		const [crateCount, fromStack, toStack] = instruction
+			.match(/\d+/g)
+			.map(Number);
+
+		return {crateCount, fromStack: fromStack - 1, toStack: toStack - 1};
+	});
+
+for (const {crateCount, fromStack, toStack} of instructions) {
+	stacks[toStack] = stacks[toStack].concat(
+		stacks[fromStack].splice(-crateCount, crateCount),
+	);
+}
+
+console.log(stacks.map((stack) => stack[stack.length - 1]).join(''));
